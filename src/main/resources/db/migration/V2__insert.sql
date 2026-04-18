f3140649fda804c27faf6f860c13661921dac623
Juan Martín Sánchez <140517935+Palmxl@users.noreply.github.com>
2026-04-17 21:43:06 -0500

    Change INSERT statement to include ID column

--- src/main/resources/db/migration/V2__insert.sql
+++ src/main/resources/db/migration/V2__insert.sql
@@ -1,6 +1,6 @@
-INSERT INTO election (name, status, start_date, end_date) VALUES
-('Elección Presidencial 2026', 'ACTIVE', NOW(), NOW() + INTERVAL '1 day'),
-('Elección Senado 2026', 'UPCOMING', NOW() + INTERVAL '10 days', NOW() + INTERVAL '12 days'),
-('Elección Alcaldía Bogotá 2027', 'UPCOMING', NOW() + INTERVAL '30 days', NOW() + INTERVAL '32 days'),
-('Elección Gobernación Antioquia 2027', 'UPCOMING', NOW() + INTERVAL '40 days', NOW() + INTERVAL '42 days'),
-('Consulta Popular Nacional', 'FINISHED', NOW() - INTERVAL '20 days', NOW() - INTERVAL '18 days');
+INSERT INTO election (id, name, status, start_date, end_date) VALUES
+(1, 'Elección Presidencial 2026', 'ACTIVE', NOW(), NOW() + INTERVAL '1 day'),
+(2, 'Elección Senado 2026', 'UPCOMING', NOW() + INTERVAL '10 days', NOW() + INTERVAL '12 days'),
+(3, 'Elección Alcaldía Bogotá 2027', 'UPCOMING', NOW() + INTERVAL '30 days', NOW() + INTERVAL '32 days'),
+(4, 'Elección Gobernación Antioquia 2027', 'UPCOMING', NOW() + INTERVAL '40 days', NOW() + INTERVAL '42 days'),
+(5, 'Consulta Popular Nacional', 'FINISHED', NOW() - INTERVAL '20 days', NOW() - INTERVAL '18 days');
